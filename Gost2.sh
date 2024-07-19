6e18f2c2e0e3860acb6a2c5d67881fabcd2574bf
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-07-19 21:58:51 +0330

    Update Gost2.sh

--- Gost2.sh
+++ Gost2.sh
@@ -56,6 +56,97 @@ printf "%s\n" "${options[@]}"
 # Read user input with white color
 read -p $'\e[97mYour choice: \e[0m' choice
 
+# If option 1 is selected
+echo "01. Iran"
+echo "02. Kharej"
+echo "03. uninstall"
+# Prompt user for IP addresses
+read -p "Select number : " choices
+if [ "$choices" -eq 01 ]; then
+  ipv4_address=$(curl -s https://api.ipify.org)
+  echo "Iran IPv4 is : $ipv4_address"
+  read -p "enter Kharej Ipv4: " ip_remote
+rctext='#!/bin/bash
+
+ip tunnel add 6to4tun_IR mode sit remote '"$ip_remote"' local '"$ipv4_address"'
+ip -6 addr add fd51:7b73:0b36::1/64 dev 6to4tun_IR
+ip link set 6to4tun_IR mtu 1480
+ip link set 6to4tun_IR up
+# confige tunnele GRE6 ya IPIPv6 IR
+ip -6 tunnel add GRE6Tun_IR mode ip6gre remote fd51:7b73:0b36::2 local fd51:7b73:0b36::1
+ip addr add 172.16.1.1/30 dev GRE6Tun_IR
+ip link set GRE6Tun_IR mtu 1436
+ip link set GRE6Tun_IR up
+
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD  -j ACCEPT
+echo "net.ipv4.ip_forward=1" > /etc/sysctl.conf
+sysctl -p
+'
+  sleep 0.5
+  echo "$rctext" > /etc/rc.local
+elif [ "$choices" -eq 02 ]; then
+  ipv4_address=$(curl -s https://api.ipify.org)
+  echo "Kharej IPv4 is : $ipv4_address"
+  read -p "enter Iran Ip : " ip_remote
+  rctext='#!/bin/bash
+ip tunnel add 6to4tun_KH mode sit remote '"$ip_remote"' local '"$ipv4_address"'
+ip -6 addr add fd51:7b73:0b36::2/64 dev 6to4tun_KH
+ip link set 6to4tun_KH mtu 1480
+ip link set 6to4tun_KH up
+
+ip -6 tunnel add GRE6Tun_KH mode ip6gre remote fd51:7b73:0b36::1 local fd51:7b73:0b36::2
+ip addr add 172.16.1.2/30 dev GRE6Tun_KH
+ip link set GRE6Tun_KH mtu 1436
+ip link set GRE6Tun_KH up
+
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD  -j ACCEPT
+echo "net.ipv4.ip_forward=1" > /etc/sysctl.conf
+sysctl -p
+'
+  sleep 0.5
+  echo "$rctext" > /etc/rc.local
+elif [ "$choices" -eq 03 ]; then
+sudo ip link show | awk '/6to4tun/ {split($2,a,"@"); print a[1]}' | xargs -I {} sudo ip link set {} down
+sudo ip link show | awk '/6to4tun/ {split($2,a,"@"); print a[1]}' | xargs -I {} sudo ip tunnel del {}
+sudo ip link show | awk '/GRE6Tun/ {split($2,a,"@"); print a[1]}' | xargs -I {} sudo ip link set {} down
+sudo ip link show | awk '/GRE6Tun/ {split($2,a,"@"); print a[1]}' | xargs -I {} sudo ip tunnel del {}
+sudo echo > /etc/rc.local
+echo "uninstalled successfully"
+read -p "do you want to reboot?(recommended)[y/n] : " yes_no
+	if [[ $yes_no =~ ^[Yy]$ ]] || [[ $yes_no =~ ^[Yy]es$ ]]; then
+ 		sudo reboot
+	fi
+else
+  echo "wrong input"
+fi
+
+chmod +x /etc/rc.local
+sleep 0.5
+/etc/rc.local
+
+echo "IP Kharej: fd51:7b73:0b36::2"
+echo "IP Iran: fd51:7b73:0b36::1"
+
 # If option 2 or 3 is selected
 if [ "$choice" -eq 2 ] || [ "$choice" -eq 3 ]; then
 
@@ -260,7 +351,7 @@ elif [ "$choice" -eq 5 ]; then
             echo $'\e[32mAuto Restart disabled successfully.\e[0m'
             ;;
         *)
-            echo $'\e[31mInvalid choice. Exit...\e[0m'
+            echo $'\e[31mInvalid choice. Exiting...\e[0m'
             exit
             ;;
     esac
@@ -313,7 +404,7 @@ if [ "$choice" -eq 6 ]; then
             disable_auto_clear_cache
             ;;
         *)
-            echo $'\e[31mInvalid choice. Exiting...\e[0m'
+            echo $'\e[31mInvalid choice. Exit...\e[0m'
             exit
             ;;
     esac
