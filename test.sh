dc90c164c48f5dd4bb386f15497e039ed41ab9e3
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-07-20 13:44:48 +0330

    Update test.sh

--- test.sh
+++ test.sh
@@ -107,8 +107,7 @@ fi
     # Commands to install and configure Gost
     sudo apt install wget nano -y && \
     echo $'\e[32mInstalling Gost version 3.0.0, please wait...\e[0m'
-    wget -O /tmp/gost.tar.gz https://github.com/iPmartNetwork/GOST/releases/download/v3.0.0-nightly.20240715/gost_3.0.0.tar.gz
-    echo $'\e[32mGost downloaded successfully.\e[0m' && \
+    wget -O /tmp/gost.tar.gz https://github.com/iPmartNetwork/GOST/releases/download/v3.0.0-nightly.20240715/gost.tar.gz
     tar -xvzf /tmp/gost.tar.gz -C /usr/local/bin/
     chmod +x /usr/local/bin/gost
     echo $'\e[32mGost installed successfully.\e[0m'
