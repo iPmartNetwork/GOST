bcf4f22ed7bbf99d20d22a70e9056aa447f5fbaa
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-07-31 23:51:08 +0330

    Update Gost3.sh

--- Gost3.sh
+++ Gost3.sh
@@ -97,7 +97,7 @@ function Install_ct() {
   [[ -z ${addyn} ]] && addyn="n"
   if [[ ${addyn} == [Yy] ]]; then
     rm -rf gost-linux-"$bit"-"$ct_new_ver".gz
-    wget --no-check-certificate https://github.com/iPmartNetwork/GOST/releases/tag/v2.11.5/gost-linux-"$bit"-"$ct_new_ver".gz
+    wget --no-check-certificate https://github.com/iPmartNetwork/GOST/releases/download/v2.11.5/gost-linux-amd64-2.11.5.gz
     gunzip gost-linux-"$bit"-"$ct_new_ver".gz
     mv gost-linux-"$bit"-"$ct_new_ver" gost
     mv gost /usr/bin/gost
